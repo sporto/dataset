5caf919815df401e9ceed06e8c4907c231df7b5a
Sebastian Porto <s@porto5.com>
2018-06-30 14:31:05 +1000

    Add documentation

--- src/Dataset.elm
+++ src/Dataset.elm
@@ -9,17 +9,42 @@ module Dataset
         , findMany
         )
 
+{-| Helpers for CRUD operations
 
+Types
+
+@docs Config
+
+CRUD operations
+
+@docs putOne, putMany, deleteOne, deleteMany, findOne, findMany
+
+-}
+
+
+{-| Configuration record
+
+`getId` is a function that given a record should return a unique id
+-}
 type alias Config a id =
     { getId : a -> id
     }
 
 
+{-|
+Add or update one record
+
+If the record exising it will be replaced in place.
+If the record is new it will added at the end.
+-}
 putOne : Config a id -> a -> List a -> List a
 putOne config item existingItems =
     putMany config [ item ] existingItems
 
 
+{-|
+Add or update many record
+-}
 putMany : Config a id -> List a -> List a -> List a
 putMany config newItems existingItems =
     let
@@ -54,16 +79,31 @@ replaceItem config updatedItems currentItem =
                 currentItem
 
 
+{-|
+Delete one record from the collection
+
+    deleteOne config 1 collection
+-}
 deleteOne : Config a id -> id -> List a -> List a
 deleteOne config id items =
     deleteMany config [ id ] items
 
 
+{-|
+Delete many records from the collection
+
+    deleteMany config [ 1, 2 ] collection
+-}
 deleteMany : Config a id -> List id -> List a -> List a
 deleteMany config ids items =
     items |> List.filter (itemsHasIdIn config ids >> not)
 
 
+{-|
+Find one record in the collection by id
+
+    findOne config 1 collection ==> Maybe item
+-}
 findOne : Config a id -> id -> List a -> Maybe a
 findOne config id items =
     items
@@ -71,6 +111,11 @@ findOne config id items =
         |> List.head
 
 
+{-|
+Find several records in the collection by id
+
+    findMany config [ 1, 2 ] collection ==> [..items..]
+-}
 findMany : Config a id -> List id -> List a -> List a
 findMany config ids items =
     List.filter (itemsHasIdIn config ids) items
